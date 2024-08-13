b848bed47573c611b35a619b0d1f564fb1577618
Red-54 <chileshreyash54@gmail.com>
2024-08-13 22:57:42 +0530

    Add Session State to Keep the User Logged In

--- account.py
+++ account.py
@@ -15,29 +15,64 @@ def app():
 
     st.title("Hello, Welcome to :red[Meal Master] 🍽️")
 
-    choice = st.selectbox('Login/Signup', ['Login','Sign Up'])
+    
+    if 'username' not in st.session_state:
+        st.session_state.username = ''
+    if 'email' not in st.session_state:
+        st.session_state.email = ''
+    if 'password' not in st.session_state:
+        st.session_state.password = ''
+
 
     def f():
         try:
             user = auth.get_user_by_email(email)
             st.write("Login Successful!")
+
+            st.session_state.username = user.uid
+            st.session_state.email = user.email
+
+            st.session_state.signedout = True
+            st.session_state.signout = True
+
         except:
             st.warning("Login Failed!")
+    
+    def t():
+        st.session_state.signout = False
+        st.session_state.signedout = False
+        st.session_state.username = ''
 
-    if choice == 'Login':
-        email = st.text_input('Email Address')
-        password = st.text_input('Password', type="password")
 
-        st.button('Login',on_click=f)
 
-    else:
+    if 'signedout' not in st.session_state:
+        st.session_state.signedout = False
+    
+    if 'signout' not in st.session_state:
+        st.session_state.signout = False
 
-        email = st.text_input('Email Address')
-        password = st.text_input('Password',type="password")
-        username = st.text_input('User Name')
+    if not st.session_state.signedout:
+        choice = st.selectbox('Login/Signup', ['Login','Sign Up'])
 
-        if st.button("Create My Account"):
-            user = auth.create_user(email=email,password=password,uid=username)
+        if choice == 'Login':
+            email = st.text_input('Email Address')
+            password = st.text_input('Password', type="password")
 
-            st.success("Account Created Successfully")
-            st.balloons()
+            st.button('Login',on_click=f)
+
+        else:
+
+            email = st.text_input('Email Address')
+            password = st.text_input('Password',type="password")
+            username = st.text_input('User Name')
+
+            if st.button("Create My Account"):
+                user = auth.create_user(email=email,password=password,uid=username)
+
+                st.success("Account Created Successfully")
+                st.balloons()
+
+    if st.session_state.signout:
+        st.text('Name'+st.session_state.username)
+        st.text('Email ID'+st.session_state.email)
+        st.button("Sign out", on_click=t)
