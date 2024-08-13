6c95878c7ec19b621273f0bc3089cb46bace3744
Red-54 <chileshreyash54@gmail.com>
2024-08-13 22:30:36 +0530

    Add Preferences Page to Main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit_option_menu as option_menu
 
-import account, suggestions, about
+import account, suggestions, about, customize
 
 st.set_page_config(
     page_title="Meal Master",
@@ -25,9 +25,9 @@ class MultiApp:
         with st.sidebar:
             app = option_menu.option_menu(
                 menu_title='Meal Master',
-                options=['Account','Suggestion','About'],
-                icons=['person-circle','chat-fill','info-circle-fill'],
-                menu_icon='chat-fill',
+                options=['Account','Preferences','Suggestion','About'],
+                icons=['person-circle','gear','chat-fill','info-circle-fill'],
+                menu_icon='house-heart',
                 default_index=0,#Index which is highlighted
                 styles={
                     "container": {"padding": "5!important","background-color":'black'},
@@ -39,6 +39,8 @@ class MultiApp:
         #Beware of spelling mismatch
         if app == "Account":
             account.app()
+        if app == "Preferences":
+            customize.app()
         if app == "Suggestion":
             suggestions.app()
         if app == "About":
