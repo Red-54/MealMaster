ee037d56000ce6ccfe0bf6976433b25b90751289
Red-54 <chileshreyash54@gmail.com>
2024-08-13 20:48:15 +0530

    Layout For the Account Login And SignUp

--- account.py
+++ account.py
@@ -1,4 +1,27 @@
 import streamlit as st
-
+import preferences as pf
 def app():
-    st.header("Account")
+
+    st.title("Hello, Welcome to :red[Meal Master] 🍽️")
+
+    choice = st.selectbox('Login/Signup', ['Login','Sign Up'])
+
+    if choice == 'Login':
+        email = st.text_input('Email Address')
+        password = st.text_input('Password', type="password")
+
+        st.button('Login')
+
+    else:
+
+        email = st.text_input('Email Address')
+        password = st.text_input('Password',type="password")
+        username = st.text_input('User Name')
+        preferences = st.multiselect(
+            "What are your Dietary Preferences?",pf.cuisine+pf.diet+pf.dairy+pf.grains+pf.protein+pf.flavours
+        )
+        allegries = st.multiselect(
+            "Do you have any allegries?",
+            pf.allegries
+        )
+
