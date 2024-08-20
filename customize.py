99c66fd43dd35684535764287d629ff084dd6a67
Red-54 <chileshreyash54@gmail.com>
2024-08-20 13:37:29 +0530

    Try and Except For Preferences and Allergies

--- customize.py
+++ customize.py
@@ -27,35 +27,35 @@ def app():
             
             a = conn.execute(text(f''' select count(*) from UserProfile where username = "{username}" and allergies is null;''')).scalar()
             if a:
-                st.session_state.allergies = ''
+                st.session_state.allergies = None
             else:
                 allergy = conn.execute(text(f'''select allergies from UserProfile where username = "{username}" ''')).fetchone()
                 st.session_state.allergies = allergy[0].split(", ")
             
             print(st.session_state.allergies)
-            if st.session_state.preferences == '':
-                perfer = st.multiselect(
-                "What are your Dietary Preferences?",pf.cuisine+pf.diet+pf.fruits+pf.dairy+pf.grains+pf.protein+pf.flavours+['']
-            )
-            else: 
-                perfer = st.multiselect(
-                    "What are your Dietary Preferences?",pf.cuisine+pf.diet+pf.fruits+pf.dairy+pf.grains+pf.protein+pf.flavours+[''],default=st.session_state.preferences
+            try:
+               perfer = st.multiselect(
+                    "What are your Dietary Preferences?",pf.cuisine+pf.diet+pf.fruits+pf.dairy+pf.grains+pf.protein+pf.flavours,default=st.session_state.preferences
                 )
-            
-            if st.session_state.allergies == '':
-                allegries = st.multiselect(
-                "Do you have any Allergies?",pf.allegries+['']
-            )
-            else:
-                allegries = st.multiselect(
-                    "Do you have any Allergies?",pf.allegries+[''],default=st.session_state.allergies
+            except:
+                perfer = st.multiselect(
+                    "What are your Dietary Preferences?",pf.cuisine+pf.diet+pf.fruits+pf.dairy+pf.grains+pf.protein+pf.flavours)
+
+            try:
+               allegries = st.multiselect(
+                    "Do you have any Allergies?",pf.allegries,default=st.session_state.allergies
                 )
-            
+            except:
+                allegries = st.multiselect(
+                    "Do you have any Allergies?",pf.allegries)
+
+
         if st.button("Save"):
             ps = ", ".join(perfer)
             als = ", ".join(allegries)
             conn = st.connection("meals_db",type="sql")
             with conn.session as s:
+
                 s.execute(text(f''' update UserProfile set preferences = "{ps}" where username = "{username}";'''))
                 s.execute(text(f''' update UserProfile set allergies = "{als}" where username = "{username}";'''))
                 s.commit()
